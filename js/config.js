841a5cf5a51d20601b3c081325ea1a3aef7220be
irvingbarajas <54381363+irvingbarajas@users.noreply.github.com>
2025-02-05 02:06:02 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_rFJ7sMtnb', // e.g. us-west-2_rFJ7sMtnb
         userPoolClientId: '3e36t77f6tlrdurvulfstggvgp', // e.g. 3e36t77f6tlrdurvulfstggvgp
+        clientSecret: '3e36t77f6tlrdurvulfstggvgp',
         region: 'us-west-2' // e.g. us-west-2
     },
     api: {
